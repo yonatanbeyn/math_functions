daa38e8e5fcfeba43ad1a27ea41a51045bc8014f
yonatan beyn <Natan09@yahoo.com>
2021-02-23 22:39:28 -0600

    Update mathfunc.py

--- mathfunc.py
+++ mathfunc.py
@@ -32,3 +32,12 @@ for  -1 == 1
 for  0 == 3
 for  1 == 5
 for  2 == 7"""
+# is prime number function
+def is_prime(n):
+	for i in range(2,n):
+		if not (n%i):
+			return False
+		return True
+for n in range(1,20):
+	if is_prime(n):
+		print(n)
